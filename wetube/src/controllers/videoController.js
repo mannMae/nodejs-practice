038c71d5e5840c0fac53891e6196bee960c6af06
Jaeman Kim <daga4242@gmail.com>
2023-11-06 19:06:19 +0900

    pug

--- wetube/src/controllers/videoController.js
+++ wetube/src/controllers/videoController.js
@@ -1,6 +1,7 @@
-export const trending = (request, response) => response.send('Home Page Video');
-export const see = (request, response) => response.send('watch');
-export const edit = (request, response) => response.send('edit');
+export const trending = (request, response) =>
+  response.render('home', { pageTitle: 'Home' });
+export const see = (request, response) => response.render('watch');
+export const edit = (request, response) => response.render('edit');
 export const search = (request, response) => response.send('Search');
 export const upload = (request, response) => response.send('Upload');
 export const remove = (request, response) => response.send('Delete');
