22a009a5dac01c77792dda83dbad58c8fc345c0b
Jaeman Kim <daga4242@gmail.com>
2023-11-07 18:50:51 +0900

    pug mixin

--- wetube/src/controllers/videoController.js
+++ wetube/src/controllers/videoController.js
@@ -1,5 +1,40 @@
-export const trending = (request, response) =>
-  response.render('home', { pageTitle: 'Home' });
+export const trending = (request, response) => {
+  const videos = [
+    {
+      title: 'FistVideo',
+      rating: 5,
+      comments: 3,
+      createdAt: '5 minutes ago',
+      views: 34,
+      id: 1,
+    },
+    {
+      title: 'FistVideo',
+      rating: 5,
+      comments: 3,
+      createdAt: '5 minutes ago',
+      views: 34,
+      id: 2,
+    },
+    {
+      title: 'FistVideo',
+      rating: 5,
+      comments: 3,
+      createdAt: '5 minutes ago',
+      views: 34,
+      id: 3,
+    },
+    {
+      title: 'FistVideo',
+      rating: 5,
+      comments: 3,
+      createdAt: '5 minutes ago',
+      views: 34,
+      id: 4,
+    },
+  ];
+  return response.render('home', { pageTitle: 'Home', videos });
+};
 export const see = (request, response) => response.render('watch');
 export const edit = (request, response) => response.render('edit');
 export const search = (request, response) => response.send('Search');
