efe3f6b3ec94d5215c0edbde503faeb7fa621a8d
Jaeman Kim <daga4242@gmail.com>
2023-11-08 21:55:08 +0900

    edit video and add post

--- wetube/src/controllers/videoController.js
+++ wetube/src/controllers/videoController.js
@@ -1,42 +1,58 @@
+import { response } from 'express';
+
+const videos = [
+  {
+    title: 'FirstVideo',
+    rating: 5,
+    comments: 3,
+    createdAt: '5 minutes ago',
+    views: 34,
+    id: 1,
+  },
+  {
+    title: 'SecondVideo',
+    rating: 5,
+    comments: 3,
+    createdAt: '5 minutes ago',
+    views: 34,
+    id: 2,
+  },
+  {
+    title: 'ThirdVideo',
+    rating: 5,
+    comments: 3,
+    createdAt: '5 minutes ago',
+    views: 34,
+    id: 3,
+  },
+  {
+    title: 'ForthVideo',
+    rating: 5,
+    comments: 3,
+    createdAt: '5 minutes ago',
+    views: 1,
+    id: 4,
+  },
+];
+
 export const trending = (request, response) => {
-  const videos = [
-    {
-      title: 'FistVideo',
-      rating: 5,
-      comments: 3,
-      createdAt: '5 minutes ago',
-      views: 34,
-      id: 1,
-    },
-    {
-      title: 'FistVideo',
-      rating: 5,
-      comments: 3,
-      createdAt: '5 minutes ago',
-      views: 34,
-      id: 2,
-    },
-    {
-      title: 'FistVideo',
-      rating: 5,
-      comments: 3,
-      createdAt: '5 minutes ago',
-      views: 34,
-      id: 3,
-    },
-    {
-      title: 'FistVideo',
-      rating: 5,
-      comments: 3,
-      createdAt: '5 minutes ago',
-      views: 34,
-      id: 4,
-    },
-  ];
   return response.render('home', { pageTitle: 'Home', videos });
 };
-export const see = (request, response) => response.render('watch');
-export const edit = (request, response) => response.render('edit');
+export const watch = (request, response) => {
+  const { id } = request.params;
+  const video = videos.find((v) => String(v.id) === id);
+  return response.render('watch', { pageTitle: `${video.title}`, video });
+};
+export const getEdit = (request, response) => {
+  const { id } = request.params;
+  const video = videos.find((v) => String(v.id) === id);
+  return response.render('edit', { pageTitle: `Edit "${video.title}"`, video });
+};
+export const postEdit = () => {
+  const { id } = request.params;
+  const { title } = request.body;
+  return response.redirect(`/videos/${id}`);
+};
 export const search = (request, response) => response.send('Search');
 export const upload = (request, response) => response.send('Upload');
 export const remove = (request, response) => response.send('Delete');
--- wetube/src/routers/videoRouter.js
+++ wetube/src/routers/videoRouter.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { edit, remove, see, upload } from '../controllers/videoController';
+import {
+  remove,
+  watch,
+  upload,
+  getEdit,
+  postEdit,
+} from '../controllers/videoController';
 
 export const videoRouter = express.Router();
 
-videoRouter.get('/upload', upload);
-videoRouter.get('/:id(\\d)', see);
-videoRouter.get('/:id(\\d)/edit', edit);
-videoRouter.get('/:id(\\d)/remove', remove);
+videoRouter.get('/:id(\\d)', watch);
+videoRouter.route('/:id(\\d)/edit').get(getEdit).post(postEdit);
