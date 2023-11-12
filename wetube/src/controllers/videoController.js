0fe5800f7ef7e4ad977bd23a935ef24dd3485ec5
Jaeman Kim <daga4242@gmail.com>
2023-11-12 14:17:07 +0900

    update video

--- wetube/src/controllers/videoController.js
+++ wetube/src/controllers/videoController.js
@@ -1,44 +1,9 @@
 import { response } from 'express';
-import { videoMoel } from '../models/Video';
-
-const videos = [
-  {
-    title: 'FirstVideo',
-    rating: 5,
-    comments: 3,
-    createdAt: '5 minutes ago',
-    views: 34,
-    id: 1,
-  },
-  {
-    title: 'SecondVideo',
-    rating: 5,
-    comments: 3,
-    createdAt: '5 minutes ago',
-    views: 34,
-    id: 2,
-  },
-  {
-    title: 'ThirdVideo',
-    rating: 5,
-    comments: 3,
-    createdAt: '5 minutes ago',
-    views: 34,
-    id: 3,
-  },
-  {
-    title: 'ForthVideo',
-    rating: 5,
-    comments: 3,
-    createdAt: '5 minutes ago',
-    views: 1,
-    id: 4,
-  },
-];
+import { videoModel } from '../models/Video';
 
 export const home = async (request, response) => {
   try {
-    const videos = await videoMoel.find({});
+    const videos = await videoModel.find({});
     return response.render('home', { pageTitle: 'Home', videos });
   } catch (error) {
     console.error(error);
@@ -47,17 +12,33 @@ export const home = async (request, response) => {
 };
 export const watch = async (request, response) => {
   const { id } = request.params;
-  const video = await videoMoel.findById(id);
+  const video = await videoModel.findById(id);
+  if (video === null) {
+    return response.render('404', { pageTitle: 'Video not found' });
+  }
   return response.render('watch', { pageTitle: `${video.title}`, video });
 };
 export const getEdit = async (request, response) => {
   const { id } = request.params;
-  const video = await videoMoel.findById(id);
+  const video = await videoModel.findById(id);
+  if (!video) {
+    return response.render('404', { pageTitle: 'Video not found' });
+  }
   return response.render('edit', { pageTitle: `Edit "${video.title}"`, video });
 };
-export const postEdit = () => {
+export const postEdit = async (request, response) => {
   const { id } = request.params;
-  const { title } = request.body;
+  const { title, description, hashtags } = request.body;
+  const video = await videoModel.exists({ _id: id });
+  if (!video) {
+    return response.render('404', { pageTitle: 'Video not found' });
+  }
+
+  await videoModel.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags: videoModel.formatHashtags(hashtags),
+  });
   return response.redirect(`/videos/${id}`);
 };
 export const getUpload = (request, response) => {
@@ -66,10 +47,10 @@ export const getUpload = (request, response) => {
 export const postUpload = async (request, response) => {
   const { title, description, hashtags } = request.body;
   try {
-    await videoMoel.create({
+    await videoModel.create({
       title,
       description,
-      hashtags: hashtags.split(',').map((tag) => `#${tag}`),
+      hashtags: videoModel.formatHashtags(hashtags),
       createdAt: Date.now(),
     });
     return response.redirect('/');
--- wetube/src/models/Video.js
+++ wetube/src/models/Video.js
@@ -8,13 +8,17 @@ const videoSchema = new mongoose.Schema({
     minLength: 2,
     maxLength: 20,
   },
-  dscription: { type: String, required: true, trim: true, maxLength: 140 },
+  description: { type: String, required: true, trim: true, maxLength: 140 },
   createdAt: { type: Date, required: true, default: Date.now },
   hashtags: [{ type: String, trim: true }],
   meta: {
-    views: { type: Number, defualt: 0, required: true },
-    rating: { type: Number, defualt: 0, required: true },
+    views: { type: Number, defualt: 0 },
+    rating: { type: Number, defualt: 0 },
   },
 });
 
-export const videoMoel = mongoose.model('Video', videoSchema);
+videoSchema.static('formatHashtags', (hashtags) =>
+  hashtags.split(',').map((tag) => (tag.startsWith('#') ? tag : `#${tag}`))
+);
+
+export const videoModel = mongoose.model('Video', videoSchema);
