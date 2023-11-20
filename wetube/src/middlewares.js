b2cbc7031896c069d9b76750de8bd101df081984
Jaeman Kim <daga4242@gmail.com>
2023-11-21 06:04:32 +0900

    add public middleware and protected middleware

--- wetube/src/middlewares.js
+++ wetube/src/middlewares.js
@@ -3,3 +3,17 @@ export const localsMiddleware = (request, response, next) => {
   response.locals.loggedInUser = request.session.user || {};
   next();
 };
+
+export const protectorMiddleware = (request, response, next) => {
+  if (request.session.loggedIn) {
+    return next();
+  }
+  return response.redirect('/login');
+};
+
+export const publicOnlyMiddleware = (request, response, next) => {
+  if (!request.session.loggedIn) {
+    return next();
+  }
+  return response.redirect('/');
+};
