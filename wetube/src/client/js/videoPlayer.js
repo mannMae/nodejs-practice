2b54951be0f81b17b4c32c0cd94c5292cd5f8aed
Jaeman Kim <daga4242@gmail.com>
2023-12-10 13:40:05 +0900

    video time and fullscreen

--- wetube/src/client/js/videoPlayer.js
+++ wetube/src/client/js/videoPlayer.js
@@ -4,6 +4,13 @@ const muteButton = document.querySelector('#mute');
 const time = document.querySelector('#time');
 const volume = document.querySelector('#volume');
 
+const currentTime = document.querySelector('#currentTime');
+const totalTime = document.querySelector('#totalTime');
+const timeline = document.querySelector('#timeline');
+
+const fullScreenButton = document.querySelector('#fullScreen');
+const videoContainer = document.querySelector('#videoContainer');
+
 const handleClickPlay = (e) => {
   if (video.paused) {
     handlePlay();
@@ -57,6 +64,53 @@ const handleVolumeChange = (event) => {
   video.volume = value;
 };
 
+const formatTime = (milliseconds) => {
+  return new Date(milliseconds * 1000).toISOString().substr(11, 8);
+};
+
+const handleLoadedMetadata = () => {
+  const duration = Math.floor(video.duration);
+  totalTime.innerText = formatTime(duration);
+  timeline.max = duration;
+};
+
+const handleTimeUpdate = () => {
+  const current = Math.floor(video.currentTime);
+  currentTime.innerText = formatTime(current);
+  timeline.value = current;
+};
+
+const handleTimelineChange = (event) => {
+  const {
+    target: { value },
+  } = event;
+  video.currentTime = value;
+};
+
+const handleClickFullScreen = () => {
+  // video.requestFullscreen();
+  const currentFullscreen = document.fullscreenElement;
+  if (!currentFullscreen) {
+    handleActivateFullScreen();
+  } else {
+    handleDeactivateFullScreen();
+  }
+};
+
+const handleActivateFullScreen = () => {
+  videoContainer.requestFullscreen();
+  fullScreenButton.innerText = 'Finish Full Screen';
+};
+
+const handleDeactivateFullScreen = () => {
+  document.exitFullscreen();
+  fullScreenButton.innerText = 'Full Screen';
+};
+
 playButton.addEventListener('click', handleClickPlay);
 muteButton.addEventListener('click', handleClickMute);
 volume.addEventListener('input', handleVolumeChange);
+video.addEventListener('loadedmetadata', handleLoadedMetadata);
+video.addEventListener('timeupdate', handleTimeUpdate);
+timeline.addEventListener('input', handleTimelineChange);
+fullScreenButton.addEventListener('click', handleClickFullScreen);
