f7524d918ad021dc12b1c54f2670461468d8c16c
Jaeman Kim <daga4242@gmail.com>
2023-12-21 00:03:41 +0900

    add recorder

--- wetube/src/client/js/recorder.js
+++ wetube/src/client/js/recorder.js
@@ -1,13 +1,41 @@
 const startButton = document.querySelector('#startRecordButton');
 const video = document.querySelector('#preview');
 
-const handleClickStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+let recorder;
+
+const handleClickStart = () => {
+  if (recorder) {
+    handleStop();
+  } else {
+    handleStart();
+  }
+};
+
+const handleStart = () => {
+  startButton.innerText = 'Stop Recording';
+  recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (e) => {
+    console.log(e.data);
+  };
+  recorder.start();
+};
+
+const handleStop = () => {
+  startButton.innerText = 'Start Recording';
+  recorder.stop();
+  recorder = null;
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
   });
   video.srcObject = stream;
   video.play();
 };
+
+init();
 
 startButton.addEventListener('click', handleClickStart);
