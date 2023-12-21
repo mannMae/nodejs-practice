727bc13c2e53100e684fcdad4e8bf2531a872a13
Jaeman Kim <daga4242@gmail.com>
2023-12-22 00:22:57 +0900

    record and download

--- wetube/src/client/js/recorder.js
+++ wetube/src/client/js/recorder.js
@@ -3,8 +3,12 @@ const video = document.querySelector('#preview');
 
 let stream;
 let recorder;
+let videoFile;
 
 const handleClickStart = () => {
+  if (videoFile) {
+    handleDownload();
+  }
   if (recorder) {
     handleStop();
   } else {
@@ -14,20 +18,37 @@ const handleClickStart = () => {
 
 const handleStart = () => {
   startButton.innerText = 'Stop Recording';
-  recorder = new MediaRecorder(stream);
-  recorder.ondataavailable = (e) => {
-    console.log(e.data);
+  recorder = new MediaRecorder(
+    stream
+    // ,{ mimeType: 'video/mp4' }
+  );
+  recorder.ondataavailable = (event) => {
+    videoFile = URL.createObjectURL(event.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
   };
   recorder.start();
 };
 
+const handleDownload = () => {
+  const anchor = document.createElement('a');
+  anchor.href = videoFile;
+  anchor.download = 'MyRecording.webm';
+  document.body.appendChild(anchor);
+  anchor.click();
+  videoFile = null;
+};
+
 const handleStop = () => {
-  startButton.innerText = 'Start Recording';
+  startButton.innerText = 'Download Recording';
   recorder.stop();
   recorder = null;
 };
 
 const init = async () => {
+  console.log(navigator);
   stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
