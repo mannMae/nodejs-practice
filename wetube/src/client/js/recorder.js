bbc1f1f54beb4ad807d9be80a628edb61cf267c3
Jaeman Kim <daga4242@gmail.com>
2023-12-23 16:59:23 +0900

    trans to mp4 with ffmpeg

--- wetube/src/client/js/recorder.js
+++ wetube/src/client/js/recorder.js
@@ -1,3 +1,5 @@
+import FFmpeg, { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+
 const startButton = document.querySelector('#startRecordButton');
 const video = document.querySelector('#preview');
 
@@ -32,13 +34,36 @@ const handleStart = () => {
   recorder.start();
 };
 
-const handleDownload = () => {
+const handleDownload = async () => {
+  console.log('download!');
+  const ffmpeg = createFFmpeg({ log: true });
+  console.log('create!');
+  await ffmpeg.load();
+  console.log('Load!');
+
+  ffmpeg.FS('writeFile', 'recording.webm', await fetchFile(videoFile));
+  console.log('File System');
+
+  await ffmpeg.run('-i', 'recording.webm', '-r', '60', 'output.mp4');
+  console.log('Run');
+
+  const mp4File = ffmpeg.FS('readFile', 'output.mp4');
+  console.log(mp4File);
+
+  const mp4Blob = new Blob([mp4File.buffer], { type: 'video/mp4' });
+
+  const mp4Url = URL.createObjectURL(mp4Blob);
+
+  console.log(mp4Url);
   const anchor = document.createElement('a');
-  anchor.href = videoFile;
-  anchor.download = 'MyRecording.webm';
+  // anchor.href = videoFile;
+  // anchor.download = 'MyRecording.webm';
+  anchor.href = mp4Url;
+  anchor.download = 'MyRecording.mp4';
   document.body.appendChild(anchor);
   anchor.click();
   videoFile = null;
+  console.log(anchor);
 };
 
 const handleStop = () => {
